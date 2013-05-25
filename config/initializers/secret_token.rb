bee2f05b6d1e7ac88e16fe9018983efbc1a16ebf
Chris Castiglione <stiggynet@Chriss-MacBook-Pro-3.local>
2013-05-24 23:08:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PfnpSite::Application.config.secret_token = '1aaab4742425bec0966f404e38a864c530237f4dfd30449ff1dc71340f4f76f2532f2ed72165276de077bdddc864f88a7c4cbaf6a8145d53611a399323ef5e43'
